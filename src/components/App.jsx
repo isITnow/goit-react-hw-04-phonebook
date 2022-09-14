26a833a31e362bd537ad0f6625abd702c61fdff5
Roman Serediuk <box4rom@gmail.com>
2022-09-14 18:33:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // import React, { Component } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Form } from './Form';
 import { ContactsList } from './ContactsList';
