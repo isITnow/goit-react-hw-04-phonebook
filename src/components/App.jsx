e1c72b31f26c9b3d3766546bfb7acf381cc3267c
Roman Serediuk <box4rom@gmail.com>
2022-09-14 19:54:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
