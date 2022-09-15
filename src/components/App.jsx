dc9a5da1174f11cd9eb467101666a7a9538a0148
Roman Serediuk <box4rom@gmail.com>
2022-09-15 19:15:41 +0300

    add initial data to contacts-list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,23 @@ import { ContactsList } from './ContactsList';
 import { Filter } from './Filter';
 
 const CONTACTS_LOCALSTORAGE_KEY = 'contacts';
+const initContacts = [
+  { id: 'id-1', name: 'Anakin Skywalker', number: '459-12-56' },
+  { id: 'id-2', name: 'Luke Skywalker', number: '443-89-12' },
+  { id: 'id-3', name: 'Leia Organa', number: '645-17-79' },
+  { id: 'id-4', name: 'Han Solo', number: '227-91-26' },
+  { id: 'id-5', name: 'Darth Sidious', number: '277-71-76' },
+  { id: 'id-6', name: 'Kyle Reese', number: '557-78-76' },
+  { id: 'id-7', name: 'Sarah Connor', number: '800-91-26' },
+  { id: 'id-8', name: 'John Connor', number: '100-09-92' },
+];
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem(CONTACTS_LOCALSTORAGE_KEY)) ?? [];
+    return (
+      JSON.parse(localStorage.getItem(CONTACTS_LOCALSTORAGE_KEY)) ??
+      initContacts
+    );
   });
   const [filter, stFilter] = useState('');
 
